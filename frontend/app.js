49d70e40f00dbd7875d0117cbfe820bfb0aee16f
Kevin <kevin.beziat.31650@gmail.com>
2025-04-07 21:56:53 +0200

    liaison entre le front et le render

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 document.getElementById('get-joke-btn').addEventListener('click', () => {
-  fetch('http://localhost:3000/api/v1/blagues/random')  // URL de ton API
+  fetch('https://simplontest.onrender.com/api/v1/blagues/random')  // URL de ton API
     .then(response => response.json())
     .then(data => {
       document.getElementById('joke-content').textContent = data.content;
